074eaa4b6e4c72c06d56057cbcb88b0aba41d983
rafaelrossetti23 <rafaelvinicius2310@gmail.com>
2023-08-29 13:14:50 -0300

    inserido sheet monkey noform

--- app/components/form.jsx
+++ app/components/form.jsx
@@ -5,11 +5,13 @@ const Form = () => {
     return (
         <div className={styles.container_form}>
             <div className={styles.element_form}>
-                <form action="#">
+                <form action="https://api.sheetmonkey.io/form/nmLMLJcn7zPcDZMVABqKYv" method="POST">
                     <h2>Conheca mais sobre nossos servicos:</h2>
-                    <input type="text" name="name" id="name" placeholder="Nome"/>
-                    <input type="email" name="email" id="email" placeholder="Email"/>
+                    <input type="text" name="Name" id="Name" placeholder="Nome" required />
+                    <input type="email" name="Email" id="Email" placeholder="Email" required />
+                    <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
                     <button type="submit">Fale Conosco</button>
+                    
                 </form>
             </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Header from './components/header.js'
 import Services from './components/services.js'
 import Apresentation from './components/apresentation.js'
 import Form from './components/form'
+
 export default function Home() {
   return (
     <div className={styles.container}>
